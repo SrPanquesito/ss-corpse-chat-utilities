e3ffeb8938129eb7d9ee0314f75c672a0ecafb5c
Luis Diaz <v-lureynoso@expediagroup.com>
2024-08-14 20:06:40 -0700

    Created mysql dump. Need to run the file in live server to migrate data

---
+++ mysql.dump.script.js
@@ -0,0 +1,22 @@
+require('dotenv').config();
+const fs = require('fs')
+const spawn = require('child_process').spawn
+const dumpFileName = `${Math.round(Date.now() / 1000)}.dump.sql`
+
+const writeStream = fs.createWriteStream('./mysql_dump/' + dumpFileName)
+
+const dump = spawn(process.env.DUMP_PATH, [
+    '-u',
+    process.env.MYSQL_USERNAME,
+    '-p' + process.env.MYSQL_PASSWORD,
+    process.env.MYSQL_DBNAME,
+])
+
+dump.stdout
+    .pipe(writeStream)
+    .on('finish', function () {
+        console.log('Completed')
+    })
+    .on('error', function (err) {
+        console.log(err)
+    })
